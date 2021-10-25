ee7c5ba8c4ac7ab7a58ad1cb91f4a2b2eeedd7f7
shafayet-hossain <shafayethossaing4609@gmail.com>
2021-10-25 20:03:54 +0600

    converted ASCII character into number

--- Keyword, Variable, data type (part-1)/ASCII.c
+++ Keyword, Variable, data type (part-1)/ASCII.c
@@ -1,8 +1,18 @@
 #include<stdio.h>
 int main()
+/*
+//How to convert ASCII number into Character
 {
     int n;
     printf("Enter Any ASCII Value : ");
     scanf("%d", &n);
     printf("The Character Is : %c", n);
+}
+*/
+//How to convert ASCII character into number
+{
+    char n;
+    printf("Enter Any ASCII Character : ");
+    scanf("%c", &n);
+    printf("The Character Is : %d", n);
 }
